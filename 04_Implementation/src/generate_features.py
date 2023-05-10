6c27677da3c98172cc8c21d7c6c4d5cb34e0bac2
Ruben Nakano <ruben.nakano@gmail.com>
2023-05-10 15:09:17 -0500

    Change to .get

--- 04_Implementation/src/generate_features.py
+++ 04_Implementation/src/generate_features.py
@@ -20,13 +20,13 @@ def generate_features(clean_data: pd.DataFrame, feature_config: dict) -> pd.Data
     features = clean_data.copy()
 
     # Drop specified columns
-    features = drop_columns(features, feature_config['drop_columns'])
+    features = drop_columns(features, feature_config.get('drop_columns', []))
 
     # Drop airlines with less than 'min_flights' flights
-    features = filter_airlines(features, feature_config['filter_airlines'])
+    features = filter_airlines(features, feature_config.get('filter_airlines', 1000))
 
     # Log transform specified columns
-    features = log_transform(features, feature_config['log_transform'])
+    features = log_transform(features, feature_config.get('log_transform', []))
 
     return features
 
