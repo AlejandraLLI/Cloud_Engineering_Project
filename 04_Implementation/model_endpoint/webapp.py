fe7103905ed6df163dd9908354b3c6e71467b333
Bannasorn Paspanthong <bannasornpaspanthong2023@u.northwestern.edu>
2023-05-10 22:16:20 -0500

    add input validation logic to webapp

--- 04_Implementation/model_endpoint/webapp.py
+++ 04_Implementation/model_endpoint/webapp.py
@@ -28,14 +28,13 @@ flight_number = st.text_input(
 
 # seat type
 st.write("Which seat do you want?")
-seat = None
 col1, col2 = st.columns(2)
 with col1:
     if st.button("Economy", use_container_width=True):
-        seat = "Economy"
+        st.session_state["seat"] = "Economy"
 with col2:
     if st.button("Business", use_container_width=True):
-        seat = "Business"
+        st.session_state["seat"] = "Business"
 
 # depart and arrive time
 departure_time = st.selectbox(
@@ -71,52 +70,79 @@ duration = st.text_input(
 # number of stops
 st.write("How many stops do you have?")
 col1, col2, col3 = st.columns(3)
-stop = None
 with col1:
     if st.button("0", use_container_width=True):
-        stop = 0  
+        st.session_state["stop"] = 0
 with col2:
     if st.button("1", use_container_width=True):
-        stop = 1      
+        st.session_state["stop"] = 1    
 with col3:
     if st.button("2", use_container_width=True):
-        stop = 2
+        st.session_state["stop"] = 2
         
 # --------VALIDATE INPUT------------
+def validate_input():
+    warnings = []
+    # TODO: validate flight number
+    # seat
+    if "seat" not in st.session_state:
+        warnings.append("Please select a seat type.")
+    # origin and destination should be different
+    if origin == destination:
+        warnings.append("Origin and destination cities must be different.")
+    # duration must exist
+    if duration == "":
+        warnings.append("Please specify the duration of your flight.")
+    # duration must be positive
+    elif float(duration) <= 0:
+        warnings.append("Duration of you flight must be a positive number.")
+    # stop
+    if "stop" not in st.session_state:
+        warnings.append("Please specify how many stops is your travel.")
+    # combine warnings
+    if warnings:
+        for warning in warnings:
+            st.warning(warning)
+        return False
+    return True
+    
 
 # --------PREDICTION-------------
 
 def predict():
-    url = "http://127.0.0.1:5000/predict"
     
-    input_data = {
-        'airline': airline,
-        'flight': flight_number,
-        'class': seat,
-        'departure_time': departure_time,
-        'origin': origin,
-        'duration': duration,
-        'stops': stop,
-        'arrival_time': arrival_time,
-        'destination': destination
-    }
-    try:
-        response = requests.post(url, json=input_data)
-        response.raise_for_status()  # Check if the request was successful
+    # validate input before making prediction
+    if validate_input():
+        url = "http://127.0.0.1:5000/predict"
+        
+        input_data = {
+            'airline': airline,
+            'flight': flight_number,
+            'class': st.session_state["seat"],
+            'departure_time': departure_time,
+            'origin': origin,
+            'duration': duration,
+            'stops': st.session_state["stop"],
+            'arrival_time': arrival_time,
+            'destination': destination
+        }
         try:
-            predicted_price = json.loads(response.text)['prediction']
-        except json.JSONDecodeError:
-            print("Error: Invalid JSON response received.")
-            print("Response text:", response.text)
-        except KeyError:
-            print("Error: 'prediction' key not found in the JSON response.")
-            print("Response text:", response.text)
-        else:
-            with st.container():
-                st.success(f"Predicted Price: {predicted_price}")
-            print("Predicted price:", predicted_price)
-    except requests.exceptions.RequestException as e:
-        print("Error: Request failed.")
-        raise e
+            response = requests.post(url, json=input_data)
+            response.raise_for_status()  # Check if the request was successful
+            try:
+                predicted_price = json.loads(response.text)['prediction']
+            except json.JSONDecodeError:
+                st.error("Error: Invalid JSON response received.")
+                st.error("Response text:", response.text)
+            except KeyError:
+                st.error("Error: 'prediction' key not found in the JSON response.")
+                st.error("Response text:", response.text)
+            else:
+                st.success(f"Predicted Price: {round(predicted_price, 2)} Rupees")
+        except requests.exceptions.RequestException as e:
+            st.error("Error: Request failed.")
+            raise e
+    else:
+        pass
 
 st.button("Predict", on_click=predict)
