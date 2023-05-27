45cfc7790ed6b1d557eb73306234f1308ac281a0
Ruben Nakano <ruben.nakano@gmail.com>
2023-05-27 10:50:41 -0500

    Add docstring

--- 04_Implementation/pipeline.py
+++ 04_Implementation/pipeline.py
@@ -1,3 +1,17 @@
+"""
+This script orchestrates the entire pipeline by calling modules that 
+perform each of the following steps:
+
+    (1) Download raw data from S3 and save it locally
+    (2) Clean the raw data and save it locally
+    (3) Generate features from the cleaned data
+    (4) Train models using the generated features
+    (5) Evaluate the trained models and save the results locally
+    (6) Upload all artifacts to S3
+
+The pipeline can be configured using the file called 'default-config.yaml'
+"""
+
 import argparse
 import datetime
 import logging.config
@@ -13,14 +27,14 @@ import src.aws_utils as aws
 import src.train_model as tm
 import src.generate_features as gf
 
-# set up logger config for some file 
+# Set up logger config for some file
 logging.config.fileConfig("config/logging/local.conf")
 logger = logging.getLogger("airline")
 
 if __name__ == "__main__":
 
     # --- Set argparser instance to handle command line arguments ---
-    # Project description 
+    # Project description
     parser = argparse.ArgumentParser(
         description="MSiA 423 - Final project: Airline price prediction"
     )
@@ -30,7 +44,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Load configuration file for parameters and run config
-    with open(args.config, "r") as f:
+    with open(args.config, "r", encoding="utf-8") as f:
         try:
             config = yaml.load(f, Loader=yaml.FullLoader)
         except yaml.error.YAMLError as e:
@@ -39,7 +53,7 @@ if __name__ == "__main__":
             logger.info("Configuration file loaded from %s", args.config)
 
     # Access run_config key from yaml config file. If it does not exist the
-    # default is an empty dictionary.    
+    # default is an empty dictionary.
     run_config = config.get("run_config", {})
 
     # Set up output directory for saving artifacts
@@ -50,7 +64,7 @@ if __name__ == "__main__":
     # Save config file to artifacts directory for traceability
     with (artifacts / "config.yaml").open("w") as f:
         yaml.dump(config, f)
-    
+
     # Create raw data set from source, upload to S3 and save to csv
     print(config["aws_config"]["bucket_name"])
     raw_data = rd.raw_data(config["aws_config"]["bucket_name"],**config["raw_data"])
@@ -78,5 +92,5 @@ if __name__ == "__main__":
     if aws_config.get("upload", False):
         uris = aws.upload_artifacts(artifacts, aws_config)
         aws.write_list_files(uris, artifacts/"list_s3_uris.txt")
-    else: 
-        logger.info("Option to upload artifacts to S3 bucket set to false. No artifacts will be uploaded.")
+    else:
+        logger.info("Upload artifacts to S3 bucket set to false. No artifacts will be uploaded.")
