9c13571e8e5fa2c174da42961a5571e738990abc
Bannasorn Paspanthong <bannasornpaspanthong2023@u.northwestern.edu>
2023-05-27 11:36:50 -0500

    add logging and config to streamlit

--- 04_Implementation/model_endpoint/aggregate_data.py
+++ 04_Implementation/model_endpoint/aggregate_data.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import numpy as np
+import logging
+import yaml
+
+with open("04_Implementation/model_endpoint/config/webapp.yaml", "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+logger = logger = logging.getLogger("aggregate_data")
 
 def get_flight_number(df: pd.DataFrame, airline: str) -> list:
     """Get all the possible flight number from the database, given the airline
@@ -11,8 +18,14 @@ def get_flight_number(df: pd.DataFrame, airline: str) -> list:
     Returns:
         list: List of possible flight numbers for the selected airline
     """
+    
+    if airline not in config["inputs"]["airline"]:
+        logger.error("The selected airline is not in our dataset.")
+        raise ValueError("The selected airline is not in our dataset.")
+    
     df = df[df["airline"] == airline]
     lst = list(df["flight"].unique())
+    logger.info("Successfully retrieved all possible flight number for %s airline", airline)
     
     return lst
 
@@ -28,6 +41,19 @@ def get_avg_duration(df: pd.DataFrame, airline: str, origin: str, destination: s
     Returns:
         float: Average duration
     """
+    
+    if airline not in config["inputs"]["airline"]:
+        logger.error("The selected airline is not in our dataset.")
+        raise ValueError("The selected airline is not in our dataset.")
+    
+    if origin not in config["inputs"]["origin"]:
+        logger.error("The selected origin is not in our dataset.")
+        raise ValueError("The selected origin is not in our dataset.")
+    
+    if destination not in config["inputs"]["destination"]:
+        logger.error("The selected destination is not in our dataset.")
+        raise ValueError("The selected destination is not in our dataset.")
+    
     df_sliced = df[(df["airline"] == airline)
                 & (df["origin"] == origin)
                 & (df["destination"] == destination)]
@@ -36,7 +62,11 @@ def get_avg_duration(df: pd.DataFrame, airline: str, origin: str, destination: s
     
     # if no data, we use simple imputation method
     if avg_duration is np.nan:
+        logger.info("Get average duration of the entire data (no data from the specific inputs)")
         return df["duration"].mean()
+
+    logger.info("Get average duration of the %s airline, %s origin, and %s destination",
+                airline, origin, destination)
     
     return avg_duration
 
@@ -53,15 +83,34 @@ def get_arrival(df: pd.DataFrame, airline: str, origin: str, destination: str, d
     Returns:
         str: The most likely arrival time
     """
+    
+    if airline not in config["inputs"]["airline"]:
+        logger.error("The selected airline is not in our dataset.")
+        raise ValueError("The selected airline is not in our dataset.")
+    
+    if origin not in config["inputs"]["origin"]:
+        logger.error("The selected origin is not in our dataset.")
+        raise ValueError("The selected origin is not in our dataset.")
+    
+    if destination not in config["inputs"]["destination"]:
+        logger.error("The selected destination is not in our dataset.")
+        raise ValueError("The selected destination is not in our dataset.")
+    
+    if depart not in config["inputs"]["depart_time"]:
+        logger.error("The selected departure time is not in our dataset.")
+        raise ValueError("The selected departure time is not in our dataset.")
+    
     df_sliced = df[(df["airline"] == airline)
                 & (df["origin"] == origin)
                 & (df["destination"] == destination)
                 & (df["departure_time"] == depart)]
-    
+
     # if no data, we use simple imputation method
     if df_sliced.empty:
         df_sliced = df[(df["origin"] == origin)
                     & (df["destination"] == destination)
                     & (df["departure_time"] == depart)]
-        
+    logger.info("Get most frequent arrival time of the %s airline, %s origin, %s destination, and %s departure time",
+                airline, origin, destination, depart)
+    
     return df_sliced["arrival_time"].value_counts().index[0]
